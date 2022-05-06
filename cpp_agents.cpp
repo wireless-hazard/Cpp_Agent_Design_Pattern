c7b6e45b7c3ae8ddf41f8110598b296826544327
Magno <magnocostamaia@gmail.com>
2022-05-05 23:00:53 -0300

    Added: RAII thread wrapper, simplest Agent class

--- cpp_agents.cpp
+++ cpp_agents.cpp
@@ -1,11 +1,36 @@
 #include <stdio.h>
 #include <thread>
 
+
 void printing(const char *parameter){
 	printf("%s\n", parameter);
 }
 
+namespace agency {
+class thread_RAII : public std::thread{
+
+	using thread::thread; //Inherits all constructor from thread
+
+public:
+	~thread_RAII(){if(this->joinable()) this->join();}
+
+};
+
+class Agent{
+public:
+	explicit Agent(const char *parameter) : t1{printing, parameter} {
+
+	}
+	~Agent(){printf("Agent is out!!\n");}
+
+private:
+	agency::thread_RAII t1;
+};
+
+} //end namespace agency 
+
+
+
 int main(){
-	std::thread t1{printing,"this string is going to be printed"};
-	t1.join();
+	agency::Agent Smith("This string");
 }
